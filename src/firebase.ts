cc2ee4c410dbd227a710ead7ac19385b7975ef1d
Klarisa Pere <your-email@example.com>
2026-04-24 12:34:15 -0500

    finish firebase login and cloud tasks

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAjQh0h42X7zP3iPhn6mFQcYN51SX1PV1Y',
+  apiKey: 'AIzaSyAjQh0h42X7zP3iPhn6mFQcYN51SX1PVlY',
   authDomain: 'study-planner-pro-b2fb1.firebaseapp.com',
   projectId: 'study-planner-pro-b2fb1',
   storageBucket: 'study-planner-pro-b2fb1.firebasestorage.app',
   messagingSenderId: '627611639960',
-  appId: '1:627611639960:web:afb8465edd864e73e270ed',
+  appId:'1:627611639960:web:afb8465edd864e73e270ed',',
+  measurementId: 'G-5DQSK8ZSN4',
 }
 
 const app = initializeApp(firebaseConfig)
